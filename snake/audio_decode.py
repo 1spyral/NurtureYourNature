10bebe725db04533bbafbd6bd95b164fdd9d4e8f
1spyral <83098914+1spyral@users.noreply.github.com>
2025-01-18 13:19:42 -0500

    create tools

---
+++ snake/audio_decode.py
@@ -0,0 +1,22 @@
+import whisper
+
+model = whisper.load_model("base")
+options = whisper.DecodingOptions()
+
+print("Whisper model loaded")
+
+def transcribe(path):
+    result = model.transcribe(path)
+    return result["text"]
+
+def decode(path):
+    audio = whisper.load_audio(path)
+    audio = whisper.pad_or_trim(audio)
+
+    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+
+    _, probs = model.detect_language(mel)
+
+    result = whisper.decode(model, mel, options)
+
+    return result.text
